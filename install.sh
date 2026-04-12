8f44c98ec86220c1c8378e0ef286297736e98566
Gabriel Nascimento <135087316+gabrielgnsilva@users.noreply.github.com>
2026-04-12 18:41:55 -0300

    fix a problem when sometimes ufw woulnd't enable as expected

--- install.sh
+++ install.sh
@@ -576,6 +576,7 @@ function postInstallationLoop() {
   if command -v ufw >&3 && pacman -Qq ufw >&3; then
     systemctl enable ufw.service >&3
     pacman -Qq openssh >&3 && ufw allow ssh >&3
+    ufw --force enable >&3
   fi
 }
 function installationloop() {
